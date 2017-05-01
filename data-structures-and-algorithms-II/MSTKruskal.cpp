34b3230ec132834576d568f855aa75e80bfda2f1
Bartosz Pogoda <bartek.pogoda1@gmail.com>
2017-05-02 00:34:20 +0200

    some work on Kruskal's algorithm

--- data-structures-and-algorithms-II/MSTKruskal.cpp
+++ data-structures-and-algorithms-II/MSTKruskal.cpp
@@ -1,11 +1,47 @@
 #include "MSTKruskal.h"
 #include "IndirectedMatrixGraph.h"
+#include "Infinity.h"
+#include "MinimumEdgeHeap.h"
+
+bool MSTKruskal::isMST(Graph * graph) {
+	return false;
+}
+
+bool MSTKruskal::canAdd(Graph * graph, Edge * edge) {
+	return false;
+}
 
 Graph * MSTKruskal::execute(Graph * graph) {
 
 	// trees 0,1, ..., n-1 in forest
 	Graph * forest = new IndirectedMatrixGraph(graph->getSize());
 
+	// create edge heap
+	Edge** edges = new Edge*[graph->getSize() * graph->getSize()]; // mb track number of edges in Graph?
+	int e = 0;
 
+	for (size_t i = 0; i < graph->getSize(); i++) {
+		for (size_t j = 0; j < graph->getSize(); j++) {
+			int weight = graph->checkEdge(i, j);
+
+			if (weight != INF) {
+				edges[e] = new Edge(i, j, weight);
+				e++;
+			}
+		}
+	}
+
+	MinimumEdgeHeap* edgeHeap = new MinimumEdgeHeap(edges, e);
+	delete[] edges;
+
+	// ...
+	Edge* edge = nullptr;
+	while ((edge = edgeHeap->getRoot()) != nullptr && isMST(forest)) {
+		if (canAdd(forest, edge))
+			;// ...
+		else
+			delete edge;
+	}
+	
 	return forest;
 }
--- data-structures-and-algorithms-II/MSTKruskal.h
+++ data-structures-and-algorithms-II/MSTKruskal.h
@@ -2,6 +2,9 @@
 #include "MST.h"
 
 class MSTKruskal : public MST {
+private:
+	bool isMST(Graph* graph);	// may be moved to MST depending on Prim's needs
+	bool canAdd(Graph* graph, Edge* edge);
 public:
 	Graph* execute(Graph* graph);
 };
